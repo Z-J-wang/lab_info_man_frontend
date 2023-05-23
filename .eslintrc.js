75afbbe0b0f27c27da9c2615514cda18efea4f0f
Jay <1356573671@qq.com>
2023-05-23 18:01:59 +0800

    feat: 更新配置

---
+++ .eslintrc.js
@@ -0,0 +1,37 @@
+module.exports = {
+  root: true,
+
+  env: {
+    node: true
+  },
+
+  extends: ['eslint:recommended', 'plugin:prettier/recommended'],
+  plugins: [
+    // https://eslint.vuejs.org/user-guide/#why-doesn-t-it-work-on-vue-file
+    // required to lint *.vue files
+    'vue'
+  ],
+  parserOptions: {
+    parser: 'babel-eslint'
+  },
+  rules: {
+    'vue/name-property-casing': ['error', 'kebab-case'],
+    'no-console': 'off',
+    'no-debugger': 'off',
+    'no-spaced-func': 2,
+    'no-const-assign': 2,
+    'no-alert': 0,
+    'no-useless-escape': 0,
+    'no-control-regex': 0,
+    'space-before-function-paren': [0, 'always']
+  },
+
+  overrides: [
+    {
+      files: ['**/__tests__/*.{j,t}s?(x)', '**/tests/unit/**/*.spec.{j,t}s?(x)'],
+      env: {
+        jest: true
+      }
+    }
+  ]
+};
